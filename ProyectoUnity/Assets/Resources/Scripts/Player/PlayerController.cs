b0245c96e79ab42a1cc5c8f153ac029a701fcb30
kevinlarico <kevin_larico955@hotmail.com>
2018-05-02 08:37:00 -0500

    Proyecto Unity
    Subo el avance del proyecto unity

---
+++ ProyectoUnity/Assets/Resources/Scripts/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public static PlayerController instance;
+	public Vector2 vec;
+	public Vector2 vecContinuo;
+	public float yPrimero;
+	public float yContinuo;
+	public float Rotacion;
+	public GameObject PosicionBala;
+	public GameObject DireccionBala;
+	public GameObject Shoot;
+	// Use this for initialization
+	void Awake () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			vec = Input.mousePosition;
+			yPrimero = vec.y;
+		}
+		if (Input.GetMouseButton (0)) {
+			vecContinuo = Input.mousePosition;
+			yContinuo = vecContinuo.y;
+			Rotacion = yPrimero - yContinuo;
+			//-46 + 60
+			Rotacion=Mathf.Clamp(Rotacion,-46f,60f);
+			transform.rotation = Quaternion.AngleAxis (Rotacion, Vector3.forward);
+
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			Instantiate (Shoot, PosicionBala.transform.position, Quaternion.identity);
+		}
+	}
+}
---
+++ ProyectoUnity/Assets/Resources/Scripts/Player/ShootPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPlayer : MonoBehaviour {
+	public Vector2 direction;
+	public Rigidbody2D rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		speed = 15f;
+		Vector3 DireccionBala = PlayerController.instance.DireccionBala.transform.position;
+		direction=(Vector2)((DireccionBala-transform.position));
+		Vector3 dir = DireccionBala - transform.position;
+		direction.Normalize ();
+		rb.velocity = direction * speed;
+
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+	
+	void OnBecameInvisible(){
+		Destroy (this.gameObject);
+	}
+}
